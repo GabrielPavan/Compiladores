9369afbec93e494a4f52ec00e93072a91be3fdea
Gabriel Pavan <gabrielpavan2014@outlook.com>
2025-03-28 20:05:32 -0300

    add file manager

--- src/infra/FileManager.java
+++ src/infra/FileManager.java
@@ -1,5 +1,41 @@
 package infra;
 
-public class FileManager {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
+public class FileManager {
+	
+	private InputStream InputStream;
+	private InputStreamReader InputStreamReader;
+	private BufferedReader BufferedReader;
+
+	private OutputStream OutputStream;
+	private OutputStreamWriter OutputStreamWriter;
+	private BufferedWriter BufferedWriter;
+
+	public void BufferFileReader(String FilePath) throws IOException {
+		InputStream = new FileInputStream(FilePath);
+		InputStreamReader = new InputStreamReader(InputStream);
+		BufferedReader = new BufferedReader(InputStreamReader);
+	}
+
+	public void BufferFileWriter(String FilePath, boolean append) throws IOException {
+		OutputStream = new FileOutputStream(FilePath, append);
+		OutputStreamWriter = new OutputStreamWriter(OutputStream);
+		BufferedWriter = new BufferedWriter(OutputStreamWriter);
+	}
+	
+	public void closeFile() throws IOException {
+		if (BufferedReader != null)
+			BufferedReader.close();
+		if (BufferedWriter != null)
+			BufferedWriter.close();
+	}
 }
