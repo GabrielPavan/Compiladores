0e889452d3c0861ecfa1c6b991944f94961b44f3
Gabriel Pavan <gabrielpavan2014@outlook.com>
2025-04-05 11:12:14 -0300

    fix math comparators

--- src/data/LexemeData.java
+++ src/data/LexemeData.java
@@ -38,17 +38,37 @@ public class LexemeData {
 		tokensResult = new ArrayList<Integer>();
 
 		Pattern identPattern = GramaticRegex.IdentGramaticRegex;
-
-		for (String lexema : lexemeData) {
+		Pattern nintPattern = GramaticRegex.NintGramaticRegex;
+		
+		System.out.println(lexemeData);
+		
+		for (int i = 0; i < lexemeData.size(); i++) {
+			String lexema = lexemeData.get(i);
 			int cod = gramaticData.SearchToken(lexema);
 
 			switch (cod) {
 			case 0:
 				Matcher identMatcher = identPattern.matcher(lexema);
+				Matcher nintMatcher = nintPattern.matcher(lexema);
+				
 				if (identMatcher.find()) {
 					tokensResult.add(gramaticData.SearchToken("ident"));
+				} else if(nintMatcher.find()){
+					tokensResult.add(gramaticData.SearchToken("nint"));
+				} else {
+					System.err.println("Erro! - Lexema nao encotrado + " + lexema);
 				}
 				break;
+			case 25:
+			case 29:
+			case 33:
+				String nextlexema = lexema.concat(lexemeData.get(i+1));
+				int codlexema = gramaticData.SearchToken(nextlexema);
+				if(codlexema != 0) {
+					tokensResult.add(codlexema);
+					i++;
+					continue;
+				}
 			default:
 				tokensResult.add(cod);
 				break;
--- src/others/GramaticRegex.java
+++ src/others/GramaticRegex.java
@@ -3,6 +3,7 @@ package others;
 import java.util.regex.Pattern;
 
 public class GramaticRegex {
-	public static final Pattern GenericGramaticRegex = Pattern.compile("\\w+|\\d+|[;=:\\(\\)\\[\\]\\{\\},\\.]");
+	public static final Pattern GenericGramaticRegex = Pattern.compile("\\w+|\\d+|[;=:\\(\\)\\[\\]\\{\\},\\.<\\>\\+\\-\\.]");
 	public static final Pattern IdentGramaticRegex = Pattern.compile("^[a-zA-Z]+$");
+	public static final Pattern NintGramaticRegex = Pattern.compile("^(0|[1-9][0-9]{0,8})$");
 }
